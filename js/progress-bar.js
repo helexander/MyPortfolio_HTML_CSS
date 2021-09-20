006c1f883181becd46c29f25d46cf05034227b6a
helexander <jeremychee97@gmail.com>
2021-09-20 16:47:41 +1000

    current progress with nav bar

---
+++ js/progress-bar.js
@@ -0,0 +1,26 @@
+let prog = document.getElementById('progressBar');
+
+let body = document.body,
+    html = document.documentElement;
+
+let height = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+);
+
+const setProgress = () => {
+    let scrollFromTop = (html.scrollTop || body.scrollTop) + html.clientHeight;
+    let width = (scrollFromTop / height) * 100 + '%';
+
+    prog.style.width = width;
+};
+
+window.addEventListener('scroll', setProgress);
+
+setProgress();
+
+
+
